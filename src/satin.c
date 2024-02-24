f7e975dbc30c53924a311d649650e73d5d5bb5bd
Alan Stewart <alankstewart@gmail.com>
2024-02-24 15:38:44 +1100

    Split gaussian_calculation function into two

--- src/satin.c
+++ src/satin.c
@@ -5,56 +5,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <sys/time.h>
+#include <time.h>
 #include <string.h>
 #include <pthread.h>
-#include <errno.h>
+#include <stdbool.h>
 
-#define RAD    0.18
-#define RAD2   (RAD * RAD)
-#define W1     0.3
-#define DR     0.002
-#define DZ     0.04
-#define LAMBDA 0.0106
-#define PI     3.14159265358979323846
-#define AREA   (PI * RAD2)
-#define Z1     (PI * (W1 * W1) / LAMBDA)
-#define Z12    (Z1 * Z1)
-#define EXPR   (2 * PI * DR)
-#define INCR   8001
+#define RAD        0.18
+#define RAD2       (RAD * RAD)
+#define W1         0.3
+#define DR         0.002
+#define DZ         0.04
+#define LAMBDA     0.0106
+#define PI         3.14159265358979323846
+#define AREA       (PI * RAD2)
+#define Z1         (PI * (W1 * W1) / LAMBDA)
+#define Z12        (Z1 * Z1)
+#define EXPR       (2 * PI * DR)
+#define INCR       8001
+#define INPUT_FILE "pin.dat"
+#define LASER_FILE "laser.dat"
 
-int main(int argc, char** argv)
-{
-    struct timeval t1;
-    struct timeval t2;
+void calculate();
 
-    gettimeofday(&t1, NULL);
+int get_input_powers(int **input_powers);
+
+int get_laser_data(Laser **lasers);
+
+void *process(void *arg);
+
+int gaussian_calculation(int input_power, float small_signal_gain, Gaussian **gaussians);
+
+int main() {
+    struct timespec t1, t2;
+
+    clock_gettime(CLOCK_MONOTONIC, &t1);
     calculate();
-    gettimeofday(&t2, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &t2);
 
-    double elapsed_time = t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec) / 1E6;
+    double elapsed_time = (t2.tv_sec - t1.tv_sec) + (t2.tv_nsec - t1.tv_nsec) / 1E9;
     printf("The time was %.3f seconds.\n", elapsed_time);
     pthread_exit(NULL);
 }
 
-void calculate()
-{
-    unsigned int i;
+void calculate() {
+    int i;
     int rc;
-    unsigned int *input_powers;
+    int *input_powers;
     Laser *lasers;
     pthread_t *threads;
     SatinProcessArgs *process_args;
 
-    unsigned int pnum = get_input_powers(&input_powers);
-    unsigned int lnum = get_laser_data(&lasers);
+    int pnum = get_input_powers(&input_powers);
+    int lnum = get_laser_data(&lasers);
 
-    if ((threads = malloc(lnum * sizeof(pthread_t))) == NULL) {
+    if ((threads = calloc(lnum, sizeof(pthread_t))) == NULL) {
         perror("Failed to allocate memory for threads");
         exit(EXIT_FAILURE);
     }
 
-    if ((process_args = malloc(lnum * sizeof(SatinProcessArgs))) == NULL) {
+    if ((process_args = calloc(lnum, sizeof(SatinProcessArgs))) == NULL) {
         perror("Failed to allocate memory for process_args");
         exit(EXIT_FAILURE);
     }
@@ -65,7 +74,7 @@ void calculate()
         process_args[i].laser_data = lasers[i];
         rc = pthread_create(&threads[i], NULL, process, &process_args[i]);
         if (rc) {
-            printf("Failed to create thread %u: %d\n", i, rc);
+            fprintf(stderr, "Failed to create thread %d: %d\n", i, rc);
             exit(EXIT_FAILURE);
         }
     }
@@ -73,7 +82,7 @@ void calculate()
     for (i = 0; i < lnum; i++) {
         rc = pthread_join(threads[i], NULL);
         if (rc) {
-            printf("Failed to join thread %u: %d\n", i, rc);
+            fprintf(stderr, "Failed to join thread %d: %d\n", i, rc);
             exit(EXIT_FAILURE);
         }
     }
@@ -84,28 +93,26 @@ void calculate()
     free(threads);
 }
 
-unsigned int get_input_powers(unsigned int **input_powers)
-{
-    unsigned int i = 0;
-    unsigned int j = 6;
-    unsigned int *input_powers_ptr;
-    char *input_power_file = "pin.dat";
+int get_input_powers(int **input_powers) {
+    int i = 0;
+    int j = 6;
+    int *input_powers_ptr;
     FILE *fp;
 
-    if ((fp = fopen(input_power_file, "r")) == NULL) {
-        fprintf(stderr, "Failed to open %s: %s\n", input_power_file, strerror(errno));
+    if ((fp = fopen(INPUT_FILE, "r")) == NULL) {
+        fprintf(stderr, "Failed to open %s: %s\n", INPUT_FILE, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    if ((input_powers_ptr = malloc(j * sizeof(unsigned int))) == NULL) {
+    if ((input_powers_ptr = calloc(j, sizeof(int))) == NULL) {
         perror("Failed to allocate memory for input_powers_ptr");
         exit(EXIT_FAILURE);
     }
 
-    while (fscanf(fp, "%u\n", &input_powers_ptr[i]) != EOF) {
+    while (fscanf(fp, "%d\n", &input_powers_ptr[i]) != EOF) {
         i++;
         if (i == j) {
-            if ((input_powers_ptr = realloc(input_powers_ptr, (j *= 2) * sizeof(unsigned int))) == NULL) {
+            if ((input_powers_ptr = realloc(input_powers_ptr, (j *= 2) * sizeof(int))) == NULL) {
                 perror("Failed to reallocate memory for input_powers_ptr");
                 exit(EXIT_FAILURE);
             }
@@ -115,32 +122,30 @@ unsigned int get_input_powers(unsigned int **input_powers)
     *input_powers = input_powers_ptr;
 
     if (fclose(fp) == EOF) {
-        fprintf(stderr, "Failed to close %s: %s\n", input_power_file, strerror(errno));
+        fprintf(stderr, "Failed to close %s: %s\n", INPUT_FILE, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     return i;
 }
 
-unsigned int get_laser_data(Laser **lasers)
-{
-    unsigned int i = 0;
-    unsigned int j = 9;
-    char *laser_data_file = "laser.dat";
+int get_laser_data(Laser **lasers) {
+    int i = 0;
+    int j = 9;
     Laser *lasers_ptr;
     FILE *fp;
 
-    if ((fp = fopen(laser_data_file, "r")) == NULL) {
-        fprintf(stderr, "Failed to open %s: %s\n", laser_data_file, strerror(errno));
+    if ((fp = fopen(LASER_FILE, "r")) == NULL) {
+        fprintf(stderr, "Failed to open %s: %s\n", LASER_FILE, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    if ((lasers_ptr = malloc(j * sizeof(Laser))) == NULL) {
+    if ((lasers_ptr = calloc(j, sizeof(Laser))) == NULL) {
         perror("Failed to allocate memory for lasers_ptr");
         exit(EXIT_FAILURE);
     }
 
-    while (fscanf(fp, "%s %f %u %s\n", lasers_ptr[i].output_file, &lasers_ptr[i].small_signal_gain,
+    while (fscanf(fp, "%s %f %d %s\n", lasers_ptr[i].output_file, &lasers_ptr[i].small_signal_gain,
                   &lasers_ptr[i].discharge_pressure, lasers_ptr[i].carbon_dioxide) != EOF) {
         i++;
         if (i == j) {
@@ -154,17 +159,16 @@ unsigned int get_laser_data(Laser **lasers)
     *lasers = lasers_ptr;
 
     if (fclose(fp) == EOF) {
-        fprintf(stderr, "Failed to close %s: %s\n", laser_data_file, strerror(errno));
+        fprintf(stderr, "Failed to close %s: %s\n", LASER_FILE, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     return i;
 }
 
-void *process(void *arg)
-{
+void *process(void *arg) {
     time_t the_time;
-    SatinProcessArgs *process_args = (SatinProcessArgs*) arg;
+    SatinProcessArgs *process_args = (SatinProcessArgs *) arg;
     Laser laser_data = process_args->laser_data;
     char *output_file = laser_data.output_file;
     Gaussian *gaussians = NULL;
@@ -180,10 +184,11 @@ void *process(void *arg)
             "Start date: %s\nGaussian Beam\n\nPressure in Main Discharge = %dkPa\nSmall-signal Gain = %4.1f\nCO2 via %s\n\nPin\t\tPout\t\tSat. Int\tln(Pout/Pin)\tPout-Pin\n(watts)\t\t(watts)\t\t(watts/cm2)\t\t\t(watts)\n",
             ctime(&the_time), laser_data.discharge_pressure, laser_data.small_signal_gain, laser_data.carbon_dioxide);
 
-    for (unsigned int i = 0; i < process_args->pnum; i++) {
-        unsigned int gaussians_size = gaussian_calculation(process_args->input_powers[i], laser_data.small_signal_gain, &gaussians);
-        for (unsigned int j = 0; j < gaussians_size; j++) {
-            fprintf(fp, "%u\t\t%7.3f\t\t%u\t\t%5.3f\t\t%7.3f\n", gaussians[j].input_power, gaussians[j].output_power,
+    for (int i = 0; i < process_args->pnum; i++) {
+        int gaussians_size = gaussian_calculation(process_args->input_powers[i], laser_data.small_signal_gain, &gaussians);
+        for (int j = 0; j < gaussians_size; j++) {
+            fprintf(fp, "%u\t\t%7.3f\t\t%u\t\t%5.3f\t\t%7.3f\n", gaussians[j].input_power,
+                    gaussians[j].output_power,
                     gaussians[j].saturation_intensity, gaussians[j].log_output_power_divided_by_input_power,
                     gaussians[j].output_power_minus_input_power);
         }
@@ -199,51 +204,36 @@ void *process(void *arg)
     }
 
     free(gaussians);
-    printf("Created %s\n", output_file);
     return NULL;
 }
 
-unsigned int gaussian_calculation(unsigned int input_power, float small_signal_gain, Gaussian **gaussians)
-{
-    unsigned int k = 17;
+int gaussian_calculation(int input_power, float small_signal_gain, Gaussian **gaussians) {
+    int k = 17;
     double *expr1;
     Gaussian *gaussians_ptr;
 
-    if ((gaussians_ptr = malloc(k * sizeof(Gaussian))) == NULL) {
+    if ((gaussians_ptr = calloc(k, sizeof(Gaussian))) == NULL) {
         perror("Failed to allocate memory for gaussians_ptr");
         exit(EXIT_FAILURE);
     }
 
-    if ((expr1 = malloc(INCR * sizeof(double))) == NULL) {
+    if ((expr1 = calloc(INCR, sizeof(double))) == NULL) {
         perror("Failed to allocate memory for expr1");
         exit(EXIT_FAILURE);
     }
 
-    for (unsigned int i = 0; i < INCR; i++) {
+    for (int i = 0; i < INCR; i++) {
         double z_inc = ((double) i - INCR / 2) / 25;
         expr1[i] = z_inc * 2 * DZ / (Z12 + pow(z_inc, 2));
     }
 
-    double input_intensity = 2 * input_power / AREA;
-    double expr2 = small_signal_gain / 32000 * DZ;
-
-    unsigned int i = 0;
-    for (unsigned int saturation_intensity = 10000; saturation_intensity <= 25000; saturation_intensity += 1000) {
-        double expr3 = saturation_intensity * expr2;
-        double output_power = 0.0;
-        for (unsigned int r = 0; r <= 250; r++) {
-            double r1 = r / 500.0;
-            double output_intensity = input_intensity * exp(-2 * pow(r1, 2) / RAD2);
-            for (unsigned int j = 0; j < INCR; j++) {
-                output_intensity *= (1 + expr3 / (saturation_intensity + output_intensity) - expr1[j]);
-            }
-            output_power += output_intensity * EXPR * r1;
-        }
+    int i = 0;
+    for (int saturation_intensity = 10000; saturation_intensity <= 25000; saturation_intensity += 1000) {
         gaussians_ptr[i].input_power = input_power;
         gaussians_ptr[i].saturation_intensity = saturation_intensity;
-        gaussians_ptr[i].output_power = output_power;
-        gaussians_ptr[i].log_output_power_divided_by_input_power = log(output_power / input_power);
-        gaussians_ptr[i].output_power_minus_input_power = output_power - input_power;
+        gaussians_ptr[i].output_power = calculate_output_power(input_power, small_signal_gain, saturation_intensity);
+        gaussians_ptr[i].log_output_power_divided_by_input_power = log(gaussians_ptr[i].output_power / input_power);
+        gaussians_ptr[i].output_power_minus_input_power = gaussians_ptr[i].output_power - input_power;
         i++;
         if (i == k) {
             if ((gaussians_ptr = realloc(gaussians_ptr, (k *= 2) * sizeof(Gaussian))) == NULL) {
@@ -259,3 +249,31 @@ unsigned int gaussian_calculation(unsigned int input_power, float small_signal_g
     return i;
 }
 
+double calculate_output_power(int input_power, float small_signal_gain, int saturation_intensity) {
+    double *expr1;
+    if ((expr1 = calloc(INCR, sizeof(double))) == NULL) {
+        perror("Failed to allocate memory for expr1");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < INCR; i++) {
+        double z_inc = ((double) i - INCR / 2) / 25;
+        expr1[i] = z_inc * 2 * DZ / (Z12 + pow(z_inc, 2));
+    }
+
+    double input_intensity = 2 * input_power / AREA;
+    double expr2 = small_signal_gain / 32000 * DZ;
+
+    double output_power = 0.0;
+    for (int r = 0; r <= 250; r++) {
+        double r1 = r / 500.0;
+        double output_intensity = input_intensity * exp(-2 * pow(r1, 2) / RAD2);
+        for (int j = 0; j < INCR; j++) {
+            output_intensity *= (1 + saturation_intensity * expr2 / (saturation_intensity + output_intensity) - expr1[j]);
+        }
+        output_power += output_intensity * EXPR * r1;
+    }
+
+    free(expr1);
+    return output_power;
+}
--- src/satin.h
+++ src/satin.h
@@ -1,34 +1,32 @@
 /* satin.h by Alan K Stewart
  Saturation Intensity Calculation */
 
-#ifndef SATIN_H
-#define SATIN_H
+#pragma once
 
 typedef struct Laser {
     char output_file[9];
     float small_signal_gain;
-    unsigned int discharge_pressure;
+    int discharge_pressure;
     char carbon_dioxide[3];
 } Laser;
 
 typedef struct SatinProcessArgs {
-    unsigned int pnum;
-    unsigned int *input_powers;
+    int pnum;
+    int *input_powers;
     Laser laser_data;
 } SatinProcessArgs;
 
 typedef struct Gaussian {
-    unsigned int input_power;
-    unsigned int saturation_intensity;
+    int input_power;
+    int saturation_intensity;
     double output_power;
     double log_output_power_divided_by_input_power;
     double output_power_minus_input_power;
 } Gaussian;
 
 void calculate();
-unsigned int get_input_powers(unsigned int **input_powers);
-unsigned int get_laser_data(Laser **lasers);
+int get_input_powers(int **input_powers);
+int get_laser_data(Laser **lasers);
 void *process(void *arg);
-unsigned int gaussian_calculation(unsigned int input_power, float small_signal_gain, Gaussian **gaussians);
-
-#endif
+int gaussian_calculation(int input_power, float small_signal_gain, Gaussian **gaussians);
+double calculate_output_power(int input_power, float small_signal_gain, int saturation_intensity);
